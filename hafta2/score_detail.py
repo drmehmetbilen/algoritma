c5b8666d29ad3bb31106ee6da117c2e1e468668b
drmehmetbilen <mehmetbilentr@gmail.com>
2025-10-03 10:38:02 +0300

    harf notu hesaplama kodu

---
+++ hafta2/score_detail.py
@@ -0,0 +1,15 @@
+
+print("Lütfen not ortalamanızı sayı olarak giriniz.!!!")
+
+notOrtalamasi = input()
+notOrtalamasi =int(notOrtalamasi)
+
+if notOrtalamasi<50:
+    print("FF")
+elif notOrtalamasi<60:
+    print("CC")
+elif notOrtalamasi<80:
+    print("BB")
+else:
+    print("AA")
+    
